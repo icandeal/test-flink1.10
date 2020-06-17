ac86eb92ce0587a9869b7dd372acceae30a44838
yuchunfan <yuchunfan@etiantian.com>
2020-06-17 17:30:12 +0800

    增加Windows测试

--- src/main/scala/com/etiantian/bigdata/ds/TestWindows.scala
+++ src/main/scala/com/etiantian/bigdata/ds/TestWindows.scala
@@ -2,6 +2,7 @@ package com.etiantian.bigdata.ds
 
 import java.util.Properties
 
+import org.apache.flink.api.common.functions.AggregateFunction
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
@@ -55,9 +56,37 @@ object TestWindows {
       TumblingProcessingTimeWindows.of(Time.seconds(5))
     ).reduce((x, y) => (x._1, x._2 + y._2)).print()
 
-    source.keyBy(_._1)
+
+
+    source.keyBy(_._1).timeWindow(
+      Time.seconds(5)
+    ).aggregate(new MyAggregateFunction).print()
+
+
+    source.keyBy(_._1).window(
+      TumblingProcessingTimeWindows.of(Time.seconds(5))
+    ).aggregate(new MyAggregateFunction, new ProcessWindowFunction[(String, Double), (String, String, Double), String, TimeWindow] {
+      override def process(key: String, context: Context, elements: Iterable[(String, Double)], out: Collector[(String, String, Double)]): Unit = {
+        out.collect((key, context.window.getStart.toString, elements.toList(0)._2))
+      }
+    }).print()
 
     senv.execute()
 
+  }
+
+  class MyAggregateFunction extends AggregateFunction[(String, Int), (String, Int, Int), (String, Double)]{
+    override def createAccumulator(): (String, Int, Int) = (null, 0, 0)
+
+    override def add(value: (String, Int), accumulator: (String, Int, Int)): (String, Int, Int) = {
+      if (accumulator._1 == null)
+        (value._1, value._2, 1)
+      else
+        (value._1,accumulator._2 + value._2, accumulator._3 + 1)
+    }
+
+    override def getResult(accumulator: (String, Int, Int)): (String, Double) = (accumulator._1, accumulator._2.toDouble / accumulator._3)
+
+    override def merge(a: (String, Int, Int), b: (String, Int, Int)): (String, Int, Int) = (a._1, a._2 + b._2, a._3 + b._3)
   }
 }
